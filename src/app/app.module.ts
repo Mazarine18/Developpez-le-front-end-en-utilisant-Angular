089e158e91ba0765c60fc713ae2d959e802c04b6
Mazarine VARMAZ-BAYARD <mazarine.varmaz-bayard@edt.fr>
2024-01-25 15:11:15 +0100

    modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
 import { HomeComponent } from './pages/home/home.component';
+import { CountryComponent } from './pages/country/country.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StatsCardComponent } from './components/stats-card/stats-card.component';
+import { PiechartComponent } from './components/piechart/piechart.component';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { routes } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgChartsModule,
+    AppRoutingModule,
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CountryComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    StatsCardComponent,
+    PiechartComponent,
+    LineChartComponent
+  ],
+  providers: [provideRouter(routes, withComponentInputBinding()),],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
